06ffb8368ab8fe1d62797204d3505567b68e0250
HanHyunSoo <gustn4563@gmail.com>
2023-03-22 12:50:11 +0900

    Initial Project

---
+++ src/main/java/com/example/slacklogback/SlackLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.slacklogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackLogbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slacklogback/SlackLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slacklogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
